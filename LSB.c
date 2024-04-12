d34ea442c084828576118068508c2b337f84e915
Udhaya Moorthy <149321495+Udhays07@users.noreply.github.com>
2024-04-12 11:47:44 +0530

    Create LSB.c

---
+++ LSB.c
@@ -0,0 +1,14 @@
+//C program to check Least Significant Bit (LSB) of a number is set or not
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter an integer n:");
+    scanf("%d", &n);
+    if(n&1)
+        printf("The LSB of %d is set(1).", n);
+    else
+        printf("The LSB of %d is not set(o)", n);
+    return 0;
+    
+}
